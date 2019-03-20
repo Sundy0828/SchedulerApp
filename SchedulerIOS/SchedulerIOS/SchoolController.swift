029ee5e42eb2f53583e95b7c91a4c321679591dd
Jerrod Sunderland <jerrod.sunderland@gmail.com>
2019-03-20 11:51:35 -0400

    base school table

--- SchedulerIOS/SchedulerIOS/SchoolController.swift
+++ SchedulerIOS/SchedulerIOS/SchoolController.swift
@@ -14,14 +14,20 @@ class SchoolController: UITableViewController {
     var data = DataController()
     var schools: [School] = []
     
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         schools = data.getSchools()
-        
-        
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "com.codepath.schoolcell", for: indexPath)
+        cell.textLabel?.text = schools[indexPath.row].SchoolName
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return schools.count
     }
     
     
