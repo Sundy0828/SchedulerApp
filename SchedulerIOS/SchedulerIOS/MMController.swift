93f55d9e91e3e13e7c49953b655acdf2ab8e7945
Adam Webb <a.webb2@setonhill.edu>
2019-04-03 12:24:46 -0400

    working on passing code

--- SchedulerIOS/SchedulerIOS/MMController.swift
+++ SchedulerIOS/SchedulerIOS/MMController.swift
@@ -9,13 +9,37 @@
 import Foundation
 import UIKit
 
-class MMController: UIViewController {
+class MMController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
+    var data = DataController()
+    var majors: [MajorMinor] = []
+    @IBOutlet weak var majorcell: UITableViewCell!
+    @IBOutlet weak var minorcell: UITableViewCell!
+    @IBOutlet weak var MajorName: UILabel!
+    @IBOutlet weak var MinorName: UILabel!
+    @IBOutlet weak var MajorTable: UITableView!
+    @IBOutlet weak var MinorTable: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        majors = data.getMM(MajorType: true)
+        
+        MajorTable.delegate = self
+        MajorTable.dataSource = self
+    }
+      func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = MajorTable.dequeueReusableCell(withIdentifier: "com.codepath.majorcell", for: indexPath)
+        cell.textLabel?.text = majors[indexPath.row].MMName
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return majors.count
     }
     
     
+    
 }
+
+
--- SchedulerIOS/SchedulerIOS/SchoolController.swift
+++ SchedulerIOS/SchedulerIOS/SchoolController.swift
@@ -14,22 +14,43 @@ class SchoolController: UITableViewController {
     var data = DataController()
     var schools: [School] = []
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         schools = data.getSchools()
     }
     
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "com.codepath.schoolcell", for: indexPath)
         cell.textLabel?.text = schools[indexPath.row].SchoolName
+        
         return cell
+        
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return schools.count
     }
     
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let secondViewController = MMController()
+        let navigationController = UINavigationController(rootViewController: secondViewController)
+        show(navigationController, sender: nil)
+    }
+    }
     
+    func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "segue" {
+            
+
+            // perform custom segue operation.
+        }
+    }
+        //Use below to get reference,so that you can pass value
+            //segue.destinationViewController
+        //segue.sourceViewController
+
     
-}
+
